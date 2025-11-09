2bd98e4e43c568dfe9f208472c7abb0d3c994e55
SirLight <strokelight35@gmail.com>
2025-11-09 19:20:29 +0200

    added 2-index.html

--- 0x02-tailwind-css/tailwind.config.js
+++ 0x02-tailwind-css/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./*.html", "./src/**/*.{html,js}"], // ← adds root .html files
   theme: {
     extend: {},
   },
